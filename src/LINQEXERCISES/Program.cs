3517a15fcc823cfe11781a18d4aa09c90baefec6
Tamer <tamer.assaliya@gmail.com>
2021-01-27 21:06:49 +0200

    add dotnet console project

---
+++ src/LINQEXERCISES/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQEXERCISES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
